2e53c807f70fe5ab8f71aa2269c55dbb749f661e
xiejiahua <554244927@qq.com>
2017-06-02 17:39:29 +0800

    修改登入

--- app/src/main/java/com/campusstreet/common/AppConfig.java
+++ app/src/main/java/com/campusstreet/common/AppConfig.java
@@ -13,6 +13,8 @@ public final class AppConfig {
      */
     public static final String DEF_HOST = "http://www.ifireant.com";
     public static final String SERVER_HOST = "http://ifireant.com";
+//    public static final String SERVER_HOST = "http://192.168.8.99:816";
+
     public static final String AVATAR_SERVER_HOST = "http://ifireant.com/d/up/";
     public static final String PIC_EWU_SERVER_HOST = "http://ifireant.com/d/ewu/";
     public static final String PIC_HOME_BANNER_SERVER_HOST = "http://ifireant.com/d/";
